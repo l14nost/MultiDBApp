019129d7130a7582c0d4e781876c2556bb8ff485
l14nost <amirbanor@gmail.com>
2025-08-11 16:39:41 +0300

    Add javadoc for service

--- src/main/java/org/example/multidbapp/service/user/UserService.java
+++ src/main/java/org/example/multidbapp/service/user/UserService.java
@@ -5,7 +5,20 @@ import org.example.multidbapp.model.response.user.UserResponse;
 
 import java.util.List;
 
+/**
+ * Service for get information about users from all databases
+ * @author AmirB
+ */
 public interface UserService {
+    /**
+     * Get list of users from all databases
+     * @return a response containing list of all users
+     */
     GlobalResponse<List<UserResponse>> getAllUsers();
+
+    /**
+     * Get list of users from all databases filtered by name, surname or username
+     * @return a response containing list of all users matching the filters.
+     */
     GlobalResponse<List<UserResponse>> getAllUsersWithFilter(String filter);
 }
